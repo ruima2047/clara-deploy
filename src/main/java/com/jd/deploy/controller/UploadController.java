0059603a3ec0a378cb12804bf3d1c4e42b8477b2
marui26 <marui5@jd.com>
2016-08-16 18:34:17 +0800

    更新

--- src/main/java/com/jd/deploy/controller/UploadController.java
+++ src/main/java/com/jd/deploy/controller/UploadController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,66 +29,31 @@ import java.util.List;
 public class UploadController {
     private HashMap baseInfoMap;
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String upload(@RequestParam("module") String module) {
+    public String upload(HttpServletRequest request,
+                         @RequestParam("userName") String userName,
+                         @RequestParam("module") String module,
+                         @RequestParam("warehouse") List<String> warehouseList,
+                         @RequestParam("Filename") String fileName,
+                         @RequestParam("Filedata")  MultipartFile file) {
 
         System.out.println(module);
-//        boolean fileExits = false;
-//        response.setContentType("text/html");
-//
-//
-//        DiskFileItemFactory factory = new DiskFileItemFactory();
-//        ServletFileUpload upload = new ServletFileUpload(factory);
-//        try {
-//            // Parse the request to get file items.
-//            List fileItems = upload.parseRequest(request);
-//            // Process the uploaded file items
-//            Iterator i = fileItems.iterator();
-//            List<FileItem> fileItemList = new ArrayList<FileItem>();
-//            List<String> warehouseList = new ArrayList<String>();
-//            String moduleName = null;
-//            String userName = null;
-//            String description = null;
-//            while (i.hasNext()) {
-//                FileItem fileItem = (FileItem) i.next();
-//                if (fileItem.isFormField() && fileItem.getFieldName().equals("moduleName")) {
-//                    moduleName = fileItem.getString();
-//                } else if(fileItem.isFormField() && fileItem.getFieldName().equals("userName")) {
-//                    userName = fileItem.getString();
-//                }else if(fileItem.isFormField() && fileItem.getFieldName().equals("description")) {
-//                    description = fileItem.getString();
-//                }else if (fileItem.isFormField() && fileItem.getFieldName().equals("warehouse")) {
-//                    warehouseList.add(fileItem.getString());
-//
-//                } else if (!fileItem.isFormField()) {
-//                    fileExits = true;
-//                    fileItemList.add(fileItem);
-//                }
-//            }
-//            if(moduleName == null) {
-//                throw new RuntimeException("moduleName is null!");
-//            }
-//            if(warehouseList.size() == 0) {
-//                throw new RuntimeException("warehouse is null!");
-//            }
-//            List<String> filePathList = new ArrayList<String>();
-//
-//            for(String warehouse : warehouseList) {
-//                BaseInfo baseInfo = (BaseInfo)baseInfoMap.get(warehouse);
-//                String uploadPath = PathUtil.getPath(baseInfo.getRootPath(),"Release","DLL",moduleName);
-//                filePathList.add(uploadPath);
-//                for(FileItem fileItem : fileItemList) {
-//                    File file = new File(PathUtil.getPath(uploadPath,fileItem.getName()));
-//                    OutputStream os = new FileOutputStream(file);
-//                    InputStream is = fileItem.getInputStream();
-//                    byte buf[] = new byte[1024];
-//                    while(is.read(buf) > 0) {
-//                        os.write(buf,0,buf.length);
-//                    }
-//                    os.flush();
-//                    os.close();
-//                    is.close();
-//                }
-//            }
+        try {
+            List<String> filePathList = new ArrayList<String>();
+            for(String warehouse : warehouseList) {
+                BaseInfo baseInfo = (BaseInfo)baseInfoMap.get(warehouse);
+                String uploadPath = PathUtil.getPath(baseInfo.getRootPath(),"Release","DLL",module);
+                filePathList.add(uploadPath);
+                    File destFile = new File(PathUtil.getPath(uploadPath,file.getName()));
+                    OutputStream os = new FileOutputStream(destFile);
+                    InputStream is = file.getInputStream();
+                    byte buf[] = new byte[1024];
+                    while(is.read(buf) > 0) {
+                        os.write(buf,0,buf.length);
+                    }
+                    os.flush();
+                    os.close();
+                    is.close();
+            }
 //            if (!fileExits) {
 //                throw new RuntimeException("you need to choose at least one file");
 //            }
@@ -96,11 +62,11 @@ public class UploadController {
 //            writer.print("true");
 //            writer.flush();
 //            writer.close();
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        } finally {
-//
-//        }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+
+        }
         return "String";
     }
 
